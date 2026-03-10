2cadb37a6a94a890f12ffe1c13b88845740232f3
rcs19 <ryan.saputil@gmail.com>
2026-03-10 16:45:20 +0000

    core and shell functions which grab json input deck from template psi and writes new psi input deck

--- src/prism_tools.py
+++ src/prism_tools.py
@@ -11,3 +11,60 @@ Quick notes:
     - /results/transpwr.dat 13 MB
 
 """
+
+import json
+import subprocess
+from pathlib import Path
+
+def write_psi_spherical_core(tc, nc, r, filepathout="core_spherical_atbase.psi"):
+    """
+    Write a new PrismSPECT input deck .psi for a spherical core plasma with the given temperature, density and size.
+    
+    Parameters:
+    -----------
+    tc: float
+        Plasma temperature in eV
+    nc: float
+        Plasma density in cm^-3
+    r: float
+        Plasma size (radius) in cm
+    """
+    with open('data/inputs/templates/core_spherical_atbase.psi','r') as f:
+        temppsi = json.load(f)
+    newpsi = temppsi
+    newpsi["Steady-state plasma params"]["Plasma temperature"] = tc
+    newpsi["Steady-state plasma params"]["Plasma density"] = nc
+    newpsi["Steady-state plasma params"]["Plasma size"] = r
+
+    with open(filepathout,'w') as f:
+        json.dump(newpsi, f, indent=4)
+
+    return filepathout
+
+def write_psi_planar_shell(ts, ns, rhoR, filepathout="shell_planar_atbase.psi"):
+    """
+    Write a new PrismSPECT input deck .psi for a planar shell plasma with the given temperature, mass density and areal density rhoR.
+    
+    Parameters:
+    -----------
+    ts: float
+        Plasma temperature in eV
+    ns: float
+        Plasma density in cm^-3
+    rhoR: float
+        Plasma size (radius) in cm
+    """
+    with open('data/inputs/templates/shell_planar_atbase_rhoR.psi','r') as f:
+        temppsi = json.load(f) # note in line 39 "Size specification ID" = 0 for thickness l and = 1 for areal density rhoL
+    newpsi = temppsi
+    newpsi["Steady-state plasma params"]["Plasma temperature"] = ts
+    newpsi["Steady-state plasma params"]["Plasma density"] = ns
+    newpsi["Steady-state plasma params"]["Plasma size"] = rhoR
+
+    with open(filepathout,'w') as f:
+        json.dump(newpsi, f, indent=4)
+
+    return filepathout
+
+if __name__ == "__main__":
+    write_psi_planar_shell(300,3e24,0.095)
