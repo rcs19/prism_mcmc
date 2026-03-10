3ed8e9ba427d0c68167361b19bed84a1afb444c6
rcs19 <ryan.saputil@gmail.com>
2026-03-10 10:43:48 +0000

    notes on what to delete

--- src/prism_tools.py
+++ src/prism_tools.py
@@ -3,4 +3,11 @@ Functions for
 - Generating PrismSPECT workspace files .psc (input decks)
 - Running PrismSPECT, deleting unnecessary files (to save space) and grabbing spectrum output
 
+Quick notes:
+- Delete:
+    - /runname.etd 35 MB
+    - /results/lineprof.dat 24 MB
+    - /results/popul.pop 1 MB
+    - /results/transpwr.dat 13 MB
+
 """
