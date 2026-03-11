3cd2b31636381f26e36152b15b9772c36d79e7fc
rcs19 <ryan.saputil@gmail.com>
2026-03-11 12:00:56 +0000

    separated run directory and run name

--- src/prism_tools.py
+++ src/prism_tools.py
@@ -9,6 +9,7 @@ import subprocess
 import numpy as np
 from pathlib import Path
 from datetime import datetime
+from matplotlib import pyplot as plt
 
 def write_psi_spherical_core(tc, nc, rc, filepathoutpsi="core_spherical_atbase.psi"):
     """
@@ -85,6 +86,7 @@ def run_PrismSPECT(psi_filepath, run_name=None, overwrite=True, delete_aux=True)
     run_command = f"PrismSPECT -b -i {psi_filepath}"
     if run_name is not None:
         run_command = run_command + f" -o {run_name}"
+        runfolder = Path(psi_filepath.parent / run_name).mkdir(exist_ok=True)
     # if output_dir is not None: 
     #     run_command = run_command + f" -d {output_dir}" # !! doesn't work - "Run directory could not be created. Check permissions or see if directory is in use."
     if overwrite:
@@ -96,13 +98,12 @@ def run_PrismSPECT(psi_filepath, run_name=None, overwrite=True, delete_aux=True)
     output_dir = (psi_filepath.parent / run_name) if run_name is not None else (psi_filepath.parent)
     if delete_aux: # Deletes /runname.etd, /runname.log, /results/lineprof.dat, /results/popul.pop, /results/transpwr.dat    
         delete_command = f'find {output_dir}'+ r' -type f \( -name "*.etd" -o -name "*.log" -o -name "*.dat" -o -name "*.pop" \) -delete'
-        delete_psi = f'find {psi_filepath.parent}'+ r' -type f \( -name "*.psi" \) -delete'
         subprocess.run(delete_command, shell=True)
-        subprocess.run(delete_psi, shell=True)
+        subprocess.run(f'rm {psi_filepath}', shell=True)
 
     return output_dir
 
-def reduced_model(tc, nc, rc, ts, ns, rhoR, samplefolder=None):
+def reduced_model(tc, nc, rc, ts, ns, rhoR, directory=None, runname=None, delete_prism=False):
     """
     Runs the reduced spherical core + shell model. 
     Wrapper function for running core simulation and shell simulations to obtain
@@ -114,19 +115,23 @@ def reduced_model(tc, nc, rc, ts, ns, rhoR, samplefolder=None):
     The emergent intensity distribution can be broadened using a gaussian broadening function with R = E/dE, if necessary.  
     """
 
-    if samplefolder is None:
-        samplefolder = "data/" + datetime.now().strftime("%Y%m%d_%H%M%S")
-    samplefolder = Path(samplefolder)
-    samplefolder.mkdir(exist_ok=True)
+    if directory is None:
+        directory = "data/"
+    if runname is None:
+        runname = datetime.now().strftime("%Y%m%d_%H%M%S")
+    rundirectory = Path(directory) / runname
+    rundirectory.mkdir(parents=True, exist_ok=True)
 
-    psi_core_path = write_psi_spherical_core(tc, nc, rc, filepathoutpsi=samplefolder / "temp_core.psi")
-    psi_shell_path = write_psi_planar_shell(ts, ns, rhoR, filepathoutpsi=samplefolder / "temp_shell.psi")
+    psi_core_path = write_psi_spherical_core(tc, nc, rc, filepathoutpsi=rundirectory / "temp_core.psi")
+    psi_shell_path = write_psi_planar_shell(ts, ns, rhoR, filepathoutpsi=rundirectory / "temp_shell.psi")
 
     # This will now run PrismSPECT twice, first for the core simulation then the shell simulation. The output files are:
     # outputfolder/runname/runname.psc - copy of input deck
     # outputfolder/runname/runname.psr - main results file
     # outputfolder/runname/results/spect.ppd - output spectra 
+    print("Running core! {psi_core_path}")
     core_path = run_PrismSPECT(psi_core_path, run_name="core", overwrite=True, delete_aux=True)
+    print(f"Running shell! {psi_shell_path}")
     shell_path = run_PrismSPECT(psi_shell_path, run_name="shell", overwrite=True, delete_aux=True)
 
     # Load spectra and transmission from output files spect.ppd
@@ -141,18 +146,29 @@ def reduced_model(tc, nc, rc, ts, ns, rhoR, samplefolder=None):
     tr_shell_interp = np.interp(nu_core, nu_shell, tr_shell)
 
     # Calculate emergent intensity distribution
-    eid = I_core * tr_shell_interp + I_shell_interp
+    eid_y = I_core * tr_shell_interp + I_shell_interp
 
     fig, ax = plt.subplots(nrows=2, sharex=True)
     ax[0].plot(nu_core, I_core, label="Core spec")
     ax[0].plot(nu_shell, I_shell, label="Shell spec")
     ax_trans = ax[0].twinx()
     ax_trans.plot(nu_shell, tr_shell, color="black", ls="--")
-    ax[1].plot(nu_core, eid, label="EID")
+    ax[1].plot(nu_core, eid_y, label="EID")
     ax[1].plot(nu_shell, I_shell, label="Shell spec")
     ax[0].legend()
     ax[1].legend()
     plt.show()
 
+    # Save emergent intensity distribution to file
+    eid = np.array([nu_core, eid_y]).T
+    np.savetxt(rundirectory / "eid.txt", eid)
+
+    if delete_prism:
+        subprocess.run(f'rm -r {rundirectory}/core', shell=True)
+        subprocess.run(f'rm -r {rundirectory}/shell', shell=True)
+
+    return eid
+
 if __name__ == "__main__":
-    reduced_model(tc=1000, nc=1e24, rc=40e-4, ts=400, ns=25, rhoR=0.09)
+    eid = reduced_model(tc=1000, nc=1e24, rc=40e-4, ts=400, ns=25, rhoR=0.09, directory = "data/20260311/", runname = "sample1")
+    print(eid)
