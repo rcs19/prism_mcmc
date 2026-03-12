7ae69ab8ab5a7226e36f32b2182ce5aee02e8648
rcs19 <ryan.saputil@gmail.com>
2026-03-12 23:10:10 +0000

    test code for getting ysigma from local variance

--- src/spec.py
+++ src/spec.py
@@ -142,3 +142,37 @@ def calibrate_x(ydata = None, ref_idx = None, ref_eV = None ):
     else:
         print("Please provide ydata and reference points eV for calibration. Returning original xdata as indices.")
         return np.arange(len(ydata))
+
+def get_ysigma(ydata, window=5):
+    ydata_smooth = np.convolve(ydata, np.ones(window)/window, mode='same')
+    var = (ydata - ydata_smooth)**2 # residuals of each point with smoothed data
+    # variance of point is mean of var within window
+    std2 = np.convolve(var, np.ones(window), mode='same')
+    count = np.convolve(np.ones_like(ydata), np.ones(window), mode='same')
+    count = np.maximum(count,2.)
+    ydata_sigma = np.sqrt(std2/(count-1))
+
+    return ydata_sigma
+
+if __name__ == "__main__":
+    from pathlib import Path
+    import matplotlib.pyplot as plt
+    # 1. Load experimental data
+    # filepath_exp = Path("data/exp/98252_xrf4/sis_f3/sis_f3_no_cr.txt")
+    filepath_exp = Path(f"data/exp/98263_xrf4/f3/f3_no_cr.txt")
+
+    filepath_sigma = filepath_exp.parent / (filepath_exp.stem + "_sigma" + filepath_exp.suffix)
+    data_exp = pd.read_csv(filepath_exp, sep="\\s+", header=None)
+    data_exp[2] = np.loadtxt(filepath_sigma).T[1]
+    data_crop = (3430,5000)
+    # data_exp[0]    = calibrate_x(data_exp[1], ref_eV=[3683,3934,4150], ref_idx=[143.5, 252.41, 332.65]) # 98252 t4 f3
+    data_exp[0]    = calibrate_x(data_exp[1], ref_eV=[3683,3934,4150], ref_idx=[142.24, 254, 339]) # 98263 t4 f3
+    data_exp_cropped = data_exp[(data_exp[0]>data_crop[0]) & (data_exp[0]<data_crop[1])].reset_index(drop=True)
+
+    xdata, ydata, ysigma  = data_exp_cropped[0].values, data_exp_cropped[1].values, data_exp_cropped[2].values
+
+
+    fig, ax = plt.subplots()
+    ax.plot(xdata, ydata, label="Data")
+    ax.fill_between(xdata, ydata-ysigma, ydata+ysigma, color="gray", alpha=0.5, label="Data sigma")
+    plt.show()
