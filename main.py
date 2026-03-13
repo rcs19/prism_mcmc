ae39de7e8e93dbfa1730d38e8013c5feb65f6034
rcs19 <ryan.saputil@gmail.com>
2026-03-13 09:30:38 +0000

    added seed! where did this go

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from src.spec import continuum_butterworth, gaussian_broadening, calibrate_x
 from src.lineratio import get_te_ne
 from src.prism_tools import reduced_model
 
+np.random.seed(0)
+
 # Define log-likelihood, log-prior and log-probability functions for MCMC
 
 def reducedchisquared(a, ydata, ymodel, ysigma):
@@ -84,13 +86,13 @@ if __name__ == "__main__":
 
     # 2c. Define parameters, initial guess, bounds and MCMC settings
     params_initial = {'tc_kev': 1.15, 'lognc': 24.3, 'ts_kev': 0.4, 'rhoR': 0.09}
-    params_bounds  = {'tc_kev': (0.9, 1.3), 'lognc': (23.5, 25), 'ts_kev': (0.2, 0.5), 'rhoR': (0.08, 0.16)}
+    params_bounds  = {'tc_kev': (0.9, 1.3), 'lognc': (23.5, 25), 'ts_kev': (0.2, 0.5), 'rhoR': (0.08, 0.17)}
     nwalkers       = 10
-    nsteps         = 120
+    nsteps         = 150
     fitting_mask   = [(3450,4500)]
-    verbose        = False
-    directory      = "data/mcmc_run_3/"
-    savefile = "mcmc_run_3.h5"
+    verbose        = True
+    directory      = "data/mcmc_run_4/"
+    savefile = "mcmc_run_4.h5"
 
     # Initial positions of walkers
     pos = np.array([val for val in params_initial.values()]) + 0.01 * np.random.randn(nwalkers, 4) # n walkers, 4 parameters, randomise initial positions slightly
@@ -101,8 +103,8 @@ if __name__ == "__main__":
     backend  = emcee.backends.HDFBackend(savefile)
     backend.reset(nwalkers, ndim)
 
-    with Pool(processes=10) as pool:
-        sampler  = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(xdata, ydata, ysigma, fitting_mask, directory, verbose), pool=pool)
+    with Pool(processes=5) as pool:
+        sampler  = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(xdata, ydata, ysigma, fitting_mask, directory, verbose), backend=backend, pool=pool)
         sampler.run_mcmc(pos, nsteps, progress=True)
 
     # Results
@@ -150,7 +152,7 @@ if __name__ == "__main__":
     directory = Path(directory)
     fig, ax = plt.subplots()
     for file in directory.rglob("*.txt"):
-        file_params = file.stem.split("_")[1:] # extract parameters from filename
+        file_params = file.stem.split("_")[1:-1] # extract parameters from filename
         tc_kev, lognc, ts_kev, rhoR = map(float, file_params)
         if (sigma_bounds[0][0] < tc_kev < sigma_bounds[0][1] and sigma_bounds[1][0] < lognc < sigma_bounds[1][1] and sigma_bounds[2][0] < ts_kev < sigma_bounds[2][1] and sigma_bounds[3][0] < rhoR < sigma_bounds[3][1]):
             sampledata = np.loadtxt(file).T
