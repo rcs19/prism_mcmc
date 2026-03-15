a4d2847c867c8ecf72a649afd5a2403d1b209efb
rcs19 <ryan.saputil@gmail.com>
2026-03-15 15:41:36 +0000

    created adjust weights function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from scipy.optimize import minimize_scalar
 
-from src.spec import continuum_butterworth, gaussian_broadening, calibrate_x
+from src.spec import continuum_butterworth, gaussian_broadening, calibrate_x, adjust_weights
 from src.lineratio import get_te_ne
 from src.prism_tools import reduced_model
 
@@ -82,17 +82,21 @@ if __name__ == "__main__":
     data_exp[0]    = calibrate_x(data_exp[1], ref_eV=[3683,3934,4150], ref_idx=[143.5, 252.41, 332.65]) # 98252 t4 f3
     data_exp_cropped = data_exp[(data_exp[0]>data_crop[0]) & (data_exp[0]<data_crop[1])].reset_index(drop=True)
 
-    xdata, ydata, ysigma = data_exp_cropped[0].values, data_exp_cropped[1].values, data_exp_cropped[2].values
+    xdata, ydata, ysigma_original = data_exp_cropped[0].values, data_exp_cropped[1].values, data_exp_cropped[2].values
+
+    # Adjust weights for the data points in the specified regions
+    ysigma = adjust_weights(ysigma_original, regions=[(3560,3750), (3830,3990),(4070,4400)], multiplier=0.2)
+
 
     # 2c. Define parameters, initial guess, bounds and MCMC settings
     params_initial = {'tc_kev': 1.15, 'lognc': 24.3, 'ts_kev': 0.4, 'rhoR': 0.09}
-    params_bounds  = {'tc_kev': (0.9, 1.3), 'lognc': (23.5, 25), 'ts_kev': (0.2, 0.5), 'rhoR': (0.08, 0.17)}
-    nwalkers       = 10
-    nsteps         = 150
-    fitting_mask   = [(3450,4800)]
+    params_bounds  = {'tc_kev': (0.9, 1.4), 'lognc': (23.5, 25), 'ts_kev': (0.2, 0.5), 'rhoR': (0.08, 0.17)}
+    nwalkers       = 12
+    nsteps         = 140
+    fitting_mask   = [(3430,4800)]
     verbose        = True
-    directory      = "data/mcmc_run_5/"
-    savefile = "mcmc_run_5.h5"
+    directory      = "data/mcmc_run_6/"
+    savefile = "mcmc_run_6.h5"
 
     # Initial positions of walkers
     pos = np.array([val for val in params_initial.values()]) + 0.01 * np.random.randn(nwalkers, 4) # n walkers, 4 parameters, randomise initial positions slightly
